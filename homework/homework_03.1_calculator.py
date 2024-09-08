5f61d05957f403f4fe2631f1c53c7908ea16bb0a
w0d8n <i.hordiichuk@outlook.com>
2024-09-08 18:46:44 +0300

    Homework 3.1 has been added. Simple calculator.

--- homework/homework_03.1_calculator.py
+++ homework/homework_03.1_calculator.py
@@ -5,3 +5,36 @@
 # обчислює та друкує результат.
 #
 # Зробити перевірку на те, що при діленні дільник не дорівнює 0!
+
+## Оператору пропонується почергово ввести число, операцію, число. Після чого буде виконана дія над числами.
+# number_1 = int(input("Enter your first number: "))
+# oper = input('Enter your operation ( +, -, *, /): ')
+# number_2 = int(input("Enter your second number: "))
+
+### Ці два вирази створені, щоб оператор вводив значення для розрахунку в одному рядку ##
+# Цей вираз лише додає значення до значення 1 + 1 = 11
+number_1, oper, number_2 = input("Enter calculation (e.g., 5 + 3): ").split()
+# Цей вираз виконує операцію над значеннями які були введені
+number_1, number_2 = int(number_1), int(number_2)
+
+# Addition
+if oper == "+":
+    print(number_1 + number_2)
+
+# Subtraction
+elif oper == "-":
+    print(number_1 - number_2)
+
+# Multiplication
+elif oper == "*":
+    print(number_1 * number_2)
+
+# Division
+elif oper == "/":
+    if number_2 == 0:
+        print("Error! Cannot divide by 0!!!")
+    else:
+        print("result: ", (number_1 / number_2))
+
+else:
+    print("You are trying to use unsupported operator!")
