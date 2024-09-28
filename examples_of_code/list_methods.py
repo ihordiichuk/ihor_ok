ba3e37ca156bf9e6d11c21cdac28f0083ab3cbfa
ihorhordiichuk <lordwayden@gmail.com>
2024-09-28 23:46:48 +0300

    add list_methods.py

--- examples_of_code/list_methods.py
+++ examples_of_code/list_methods.py
@@ -48,8 +48,67 @@
 #############
 # 5 extend() --> like append
 
-people_1: list[str] = ['Mario', 'Elon', 'Trump', 'Elon']        # to extend list need to create another one
-people_2: list[str] = ['apple', 'banana']                       # list to be extended to first
+# people_1: list[str] = ['Mario', 'Elon', 'Trump', 'Elon']        # to extend list need to create another one
+# people_2: list[str] = ['apple', 'banana']                       # list to be extended to first
+#
+# people_1.extend(people_2)
+## people_1.extend([people_2])                          <-----    # to put list inside list
+# print(people_1)
 
-people_1.extend(people_2)
-print(people_1)
+
+#############
+# 6 index()
+
+# people: list[str] = ['Mario', 'Elon', 'Trump']
+# print(people.index('Trump'))
+
+
+#############
+# 7 insert()
+
+# people: list[str] = ['Mario', 'Elon', 'Trump']
+# print(list(range(len(people))))                         # len to calculates the length,
+#                                                         # range to create a sequence of numbers from 0
+#                                                         # list converts the range object into an actual list.
+#
+# people.insert(1, 'Luigi')                       # ( _index: position, _object: what to insert
+# print(people)
+
+
+#############
+# 8 pop()
+
+# people: list[str] = ['Mario', 'Elon', 'Trump']
+# popped = people.pop(0)
+#
+# print(people)
+# print(popped)
+
+
+#############
+# 9 remove()
+
+# people: list[str] = ['Mario', 'Elon', 'Trump']
+# people.remove('Elon')
+#
+# print(people)
+
+
+#############
+# 10 reverse()
+
+# people: list[str] = ['Mario', 'Elon', 'Trump']
+# people.reverse()
+#
+# print(people)
+
+
+#############
+# 11 sort()
+
+# people: list[str] = ['Mario', 'Elon', 'bob', 'Trump', 'yaser']
+# # people.sort()  # (self, key, reverse)
+# # people.sort(key=lambda name: len(name), reverse=True)
+# people.sort(reverse=True)
+#
+# print(people)
